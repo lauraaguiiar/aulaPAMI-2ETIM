455ade2a83e83c6a47cb7552612821e116560043
lauraaguiiar <laura.aguiar3@etec.sp.gov.br>
2024-11-05 15:21:06 -0300

    Aula 05-11-2024

---
+++ src/atividades/atv_02/index.js
@@ -0,0 +1,24 @@
+import { View, Text, Image } from 'react-native'; 
+
+import Mensagem from './mensagem';
+
+import logo from '../../../assets/002-1-react-native.png'; 
+
+import styles from './styles';
+
+export default function Atividade2 () { 
+
+    return(
+        <View style={styles.container}>
+            <Text style={styles.titulo}>
+                Atividade 2
+            </Text>
+            <Image source={logo} style={styles.imagem} /> 
+
+            <Mensagem nome={'Ewerton'} idade={56} />
+            <Mensagem nome={'Werton'} idade={23} />
+            <Mensagem nome={'Erton'} idade={19} />
+        </View>
+    );
+}
+
